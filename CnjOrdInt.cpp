f7f167755489f802a3c81f35e20d41dbc437db7a
daagonzalez <daa.gonzalez@gmail.com>
2017-02-15 14:04:14 -0600

    Agregado el constructor de copias

--- CnjOrdInt.cpp
+++ CnjOrdInt.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
  * File:   CnjOrdInt.cpp
  * Author: alan.calderon
- * 
+ *
  * Created on 5 de agosto de 2015, 05:37 PM
  */
 
@@ -11,7 +11,19 @@ CnjOrdInt::CnjOrdInt():inicio(0) {
 }
 
 CnjOrdInt::CnjOrdInt(const CnjOrdInt& orig) {
-
+  NdoInt* p = orig.inicio;
+  if (p == 0)
+      inicio = 0;
+  else {
+      inicio = new NdoInt(orig.inicio->dato);
+      p = p->sgt;
+      NdoInt* ultimo = inicio;
+      while(p != 0){ // mientras no se acabe orig.
+          ultimo->sgt = new NdoInt(p->dato);
+          p = p->sgt;
+          ultimo = ultimo->sgt;
+      }
+  }
 }
 
 CnjOrdInt::~CnjOrdInt() {
@@ -44,5 +56,5 @@ CnjOrdInt& CnjOrdInt::operator/(const CnjOrdInt& b) const {
 }
 
 string CnjOrdInt::aHil() {
-   
+
 }
