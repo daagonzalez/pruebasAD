36a6e651de48a8a861235ba66ab590d9f6e5e9a5
daagonzalez <daa.gonzalez@gmail.com>
2017-02-15 14:06:26 -0600

    Código del destructor

--- CnjOrdInt.cpp
+++ CnjOrdInt.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
  * File:   CnjOrdInt.cpp
  * Author: alan.calderon
- * 
+ *
  * Created on 5 de agosto de 2015, 05:37 PM
  */
 
@@ -15,7 +15,13 @@ CnjOrdInt::CnjOrdInt(const CnjOrdInt& orig) {
 }
 
 CnjOrdInt::~CnjOrdInt() {
-
+  NdoInt* p = inicio;
+  NdoInt* q = 0;
+  while (p != 0) {
+      q = p->sgt;
+      delete p;
+      p = q;
+  }
 }
 
 bool CnjOrdInt::insertarOrd(int x) {
@@ -44,5 +50,5 @@ CnjOrdInt& CnjOrdInt::operator/(const CnjOrdInt& b) const {
 }
 
 string CnjOrdInt::aHil() {
-   
+
 }
