139d4886ee7aef1fcaadaa5403c75405827930c1
daagonzalez <daa.gonzalez@gmail.com>
2017-02-15 14:07:11 -0600

    Eliminar dato

--- CnjOrdInt.cpp
+++ CnjOrdInt.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
  * File:   CnjOrdInt.cpp
  * Author: alan.calderon
- * 
+ *
  * Created on 5 de agosto de 2015, 05:37 PM
  */
 
@@ -23,7 +23,33 @@ bool CnjOrdInt::insertarOrd(int x) {
 }
 
 bool CnjOrdInt::eliminar(int x) {
-
+  bool rsl = false;
+  NdoInt* p = inicio;
+  NdoInt* ant = 0;
+  if ( inicio != 0 && inicio->dato <= x )
+      if ( inicio->dato == x ){
+          p = inicio->sgt;
+          delete inicio;
+          inicio = p;
+      } else {
+          while(p != 0){ // hay que buscar a x
+              if (p->dato == x){
+                  rsl = true;
+                  p = 0;
+              } else { // hay que seguir buscando
+                  if (p->dato < x){
+                      ant = p;
+                      p = p->sgt;
+                  } else p = 0; // no está y no nada que eliminar
+              }
+          }
+          if (rsl){ // sí hay que eliminar en medio o al final
+              p = ant->sgt->sgt;
+              delete ant->sgt;
+              ant->sgt = p;
+          }
+      }
+  return rsl;
 }
 
 bool CnjOrdInt::buscar(int x) const {
@@ -44,5 +70,5 @@ CnjOrdInt& CnjOrdInt::operator/(const CnjOrdInt& b) const {
 }
 
 string CnjOrdInt::aHil() {
-   
+
 }
